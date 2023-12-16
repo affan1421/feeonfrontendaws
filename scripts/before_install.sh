2e3b2c1d2fb75c34e9ff8d0bdc96493065aabaac
affan <affan.cumin@gmail.com>
2023-12-16 19:15:34 +0530

    aws64

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd /home/ubuntu /server
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-yum -y install nodejs npm
+set -e
+
+# Install Node.js
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
