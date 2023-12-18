ec5363977343e5562c6ae67e77a13c529a844db8
affan <affan.cumin@gmail.com>
2023-12-18 12:44:05 +0530

    aws73

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -5,4 +5,5 @@ set -e
 cd /home/ubuntu/server
 
 # Start your React app (assuming it's a static build)
-npm run dev
+node --loader ts-node/esm --experimental-specifier-resolution=node /home/ubuntu/server/node_modules/.bin/vite --host
+
