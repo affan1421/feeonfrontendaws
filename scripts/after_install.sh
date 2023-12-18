cd48c6b41ce3d9d68cea76f12b30a58a332ebb06
affan <affan.cumin@gmail.com>
2023-12-18 11:37:08 +0530

    aws68

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 
-# Change Working Directory
 cd /home/ubuntu/server
 
-# Install dependencies
-npm install 
-
-
+npm install --save react react-dom react-scripts react-particles-js
+npm install pm2 -g
